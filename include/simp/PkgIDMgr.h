a05359b23ba5efaf550bbf3291850ea1f92c78bf
zz <zhuguang@ejoy.com>
2017-03-04 18:15:17 +0800

    [ADDED] pkg id mgr

---
+++ include/simp/PkgIDMgr.h
@@ -0,0 +1,49 @@
+#ifndef _SIMP_PKG_ID_MGR_H_
+#define _SIMP_PKG_ID_MGR_H_
+
+#include <CU_Singleton.h>
+
+#include <bimp/FileLoader.h>
+
+#include <map>
+#include <string>
+
+namespace simp
+{
+
+class PkgIDMgr
+{
+public:
+	void LoadPkgIDs(const std::string& filepath);
+	int  QueryPkgID(const std::string& name) const;
+
+private:
+	struct PkgID
+	{
+		std::string path;
+		int         id;
+	};
+
+	class PkgIDsLoader : public bimp::FileLoader
+	{
+	public:
+		PkgIDsLoader(const std::string& filepath, std::map<std::string, PkgID>& pkgs);
+
+	protected:
+		virtual void OnLoad(bimp::ImportStream& is);
+
+	private:
+		std::map<std::string, PkgID>& m_pkgs;
+
+	}; // PkgIDsLoader
+
+private:
+	std::map<std::string, PkgID> m_pkgs;
+
+	SINGLETON_DECLARATION(PkgIDMgr);
+
+}; // PkgIDMgr
+
+}
+
+#endif // _SIMP_PKG_ID_MGR_H_
---
+++ source/PkgIDMgr.cpp
@@ -0,0 +1,57 @@
+#include "PkgIDMgr.h"
+
+#include <bimp/BIMP_ImportStream.h>
+
+namespace simp
+{
+
+SINGLETON_DEFINITION(PkgIDMgr);
+
+PkgIDMgr::PkgIDMgr()
+{
+}
+
+void PkgIDMgr::LoadPkgIDs(const std::string& filepath)
+{
+	m_pkgs.clear();
+	PkgIDsLoader loader(filepath, m_pkgs);
+	loader.Load();
+}
+
+int PkgIDMgr::QueryPkgID(const std::string& name) const
+{
+	std::map<std::string, PkgID>::const_iterator itr 
+		= m_pkgs.find(name);
+	if (itr != m_pkgs.end()) {
+		return itr->second.id;
+	} else {
+		return -1;
+	}
+}
+
+/************************************************************************/
+/* class PkgIDMgr::PkgIDsLoader                                         */
+/************************************************************************/
+
+PkgIDMgr::PkgIDsLoader::
+PkgIDsLoader(const std::string& filepath, std::map<std::string, PkgID>& pkgs)
+	: FileLoader(filepath)
+	, m_pkgs(pkgs)
+{
+}
+
+void PkgIDMgr::PkgIDsLoader::
+OnLoad(bimp::ImportStream& is)
+{
+	int n = is.UInt16();
+	for (int i = 0; i < n; ++i) 
+	{
+		std::string name = is.String();
+		PkgID pkg;
+		pkg.path = is.String();
+		pkg.id = is.UInt16();
+		m_pkgs.insert(std::make_pair(name, pkg));
+	}
+}
+
+}
