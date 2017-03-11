af4d98639e6058e21bb17b8b89c4dd474871b314
yangkaibo <yangkaiboo@163.com>
2017-03-11 17:27:22 +0800

    build android

--- Android.mk
+++ Android.mk
@@ -6,7 +6,20 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := simp
 
+LOCAL_C_INCLUDES  := \
+	${SIMP_SRC_PATH}/include \
+	${SIMP_SRC_PATH}/include/simp \
+	${BIMP_SRC_PATH}/include \
+	${CU_SRC_PATH} \
+	${LOGGER_SRC_PATH} \
+	${SM_SRC_PATH} \
+	${PS_SRC_PATH} \
+	${FS_SRC_PATH} \
+	${MTRAIL_SRC_PATH} \
+
+
 LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.cpp" -print)) \
 
 include $(BUILD_STATIC_LIBRARY)
 
