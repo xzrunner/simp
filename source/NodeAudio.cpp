611a6ec4afc44983cc14acdf746e69e6cfd0e027
zz <zhuguang@ejoy.com>
2017-11-02 16:27:14 +0800

    fix compile

--- source/NodeAudio.cpp
+++ source/NodeAudio.cpp
@@ -1,7 +1,7 @@
-#include "NodeAudio.h"
-#include "ImportStream.h"
+#include "simp/NodeAudio.h"
+#include "simp/ImportStream.h"
 
-#include <bimp/bimp_typedef.h>
+#include <bimp/typedef.h>
 
 namespace simp
 {
--- source/NodeAudioSpr.cpp
+++ source/NodeAudioSpr.cpp
@@ -1,7 +1,7 @@
-#include "NodeAudioSpr.h"
-#include "ImportStream.h"
+#include "simp/NodeAudioSpr.h"
+#include "simp/ImportStream.h"
 
-#include <bimp/bimp_typedef.h>
+#include <bimp/typedef.h>
 
 namespace simp
 {
